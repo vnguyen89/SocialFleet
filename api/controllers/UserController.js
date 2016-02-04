5589689ad4020d0e248140d2e5ebaad0be59e604
Vu Nguyen <vnguyen1989@hotmail.com.vn>
2016-02-04 15:31:11 +0800

    Init

---
+++ api/controllers/UserController.js
@@ -0,0 +1,15 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+var bcrypt = require('bcrypt');
+
+module.exports = {
+	login : function(req, res) {
+		
+	}
+};
+
